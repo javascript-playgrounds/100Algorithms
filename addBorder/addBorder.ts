961233cd021920579881aa93055d683ff4b0d5a0
zach beecher <zachbeecher@gmail.com>
2018-06-28 17:38:42 -0700

    couple more added

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,15 @@
 function addBorder(picture: string[]): string[] {
-
+    const lengthOfWall = picture[0].length + 2;
+    let wall ='';
+    for(let i=0; i<lengthOfWall; i++){
+       wall = wall.concat('*');
+    }
+    picture.unshift(wall);
+    picture.push(wall);
+for (let j=1; j<picture.length-1; j++){
+    picture[j] = '*'.concat(picture[j], '*');
+}
+    return picture;
 }
 
-// console.log(addBorder(["abc", "ded"]));
+console.log(addBorder(["abc", "ded"]));
--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,8 @@
 function addTwoDigits(n: any): number {
-
+    var nums = n.toString().split('');
+    return nums.reduce((a: string, b: string)=> {
+      return parseInt(a) + parseInt(b)
+    })
 }
 
-// console.log(addTwoDigits(29));
+console.log(addTwoDigits(29));
